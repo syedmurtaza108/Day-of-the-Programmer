964b7d269639034ef3bad3cf5d82bc8860580010
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-11-06 19:48:07 +0500

    Add project files.

---
+++ Day of the Programmer/Program.cs
@@ -0,0 +1,31 @@
+﻿//Finished in 15 minutes
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_of_the_Programmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+             
+            string result = dayOfProgrammer(year);
+            Console.WriteLine(result);
+        }
+        static string dayOfProgrammer(int year)
+        {
+            if (year == 1918)
+                return "26.09." + year.ToString();
+            else if ((year < 1918 && (year % 4 == 0)) || ((year > 1918) && (year % 4 == 0 & year % 100 != 0) || (year % 400 == 0)))
+                return "12.09." + year.ToString();
+            else
+                return "13.09." + year.ToString() ;
+
+        }
+    }
+}
